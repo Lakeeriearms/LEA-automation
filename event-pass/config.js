47de863b5c606db9cc7cb0c73963380399a53dde
Pointandshootmedia <clement@pointandshoot.media>
2026-07-22 10:23:36 -0400

    Point event pass to new Apps Script deployment

--- event-pass/config.js
+++ event-pass/config.js
@@ -1,5 +1,5 @@
 window.LEA_EVENT_CONFIG = {
-  apiUrl: "https://script.google.com/macros/s/AKfycbyaV6UjLzCUwF9HWt4NPXQzU-3SOY4m1-KxQB4S-V9IfospVbGzIbjJBeoKSXYIZNl3cg/exec",
+  apiUrl: "https://script.google.com/macros/s/AKfycbzA3cLTR4TVxOK7Houhh5R7J78YeGfpX5eRUsI_YAJv_fYZQrTxT0BpLmVDwivPlparhA/exec",
   spreadsheetId: "1XAj47kCYJ7MMK4WcsLmGhCyLbRzbMDgfMYw7RKPO_yw",
   spreadsheetUrl: "https://docs.google.com/spreadsheets/d/1XAj47kCYJ7MMK4WcsLmGhCyLbRzbMDgfMYw7RKPO_yw/edit",
   eventKey: new URLSearchParams(window.location.search).get("event") || "event-main",
